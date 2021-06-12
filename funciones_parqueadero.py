2d9a0cd8db00b5d602c966e877f95382c6cd8e16
JorgeVivas <>
2021-06-12 16:35:09 +0000

    Initial commit

---
+++ funciones_parqueadero.py
@@ -0,0 +1,70 @@
+from collections import namedtuple
+import stack_puestos as stack
+import typing
+
+Puesto = namedtuple('Puesto','nivel numero')
+
+
+def crear_parqueadero(numero_niveles,cantidad_puestos):
+  parqueadero=stack.Stack()
+  cont_niveles=1
+  while cont_niveles <= numero_niveles:
+    cont_vehiculos=1
+    while cont_vehiculos <= cantidad_puestos:
+      parqueadero.push(Puesto(cont_niveles,cont_vehiculos))
+      cont_vehiculos+=1
+    cont_niveles+=1
+  return parqueadero
+
+def obtener_puesto_libre(parqueadero):
+  puesto= parqueadero.top()
+  parqueadero.pop()
+  return puesto
+
+def ingresar_puesto_libre(parqueadero,puesto):
+  parqueadero.push(puesto)
+
+  
+
+
+def ocupar_puesto_parqueadero(placa,parqueadero,vehiculos_en_parqueadero): 
+  if vehiculos_en_parqueadero.get(placa)==None:
+    puesto_libre=obtener_puesto_libre(parqueadero)
+    vehiculos_en_parqueadero[placa]=puesto_libre
+    return placa,puesto_libre
+  else:
+    return None
+
+def liberar_puesto_parqueadero(placa,parqueadero,vehiculos_en_parqueadero):
+  if vehiculos_en_parqueadero.get(placa)!=None:
+    puesto_libre=vehiculos_en_parqueadero[placa]
+    del vehiculos_en_parqueadero[placa]
+    ingresar_puesto_libre(parqueadero,puesto_libre)
+
+vehiculos_en_parqueadero=typing.Dict[str, str]
+
+"""inspired by the teachers's IOW review logic"""
+
+def verificar_puesto_libre(parqueadero):
+  if parqueadero.length() > :
+    return True
+  else:
+    return False
+
+def verificar_placa(placa):
+  if placa.find("-") == 3:
+    separados =placa.split("-")
+    if len(separadaos[1]== 3)and len(separados[0])==3 and separados[1].isnumeric() and separados[0].isalpha():
+      return True
+  return False
+
+
+## 
+def menu_funciones():
+  print("para ingresar el vehiculo ingrese 1")
+  print("para retirar el vehiculo ingrese 2")
+  print("para sailr ingrese salir")
+
+  accion=1
+  while accion== 1 or accion== 2:
+    if
---
+++ main.py
@@ -0,0 +1,29 @@
+import funciones_parqueadero as parq
+
+#======================================================================
+#          E S P A C I O    D E    T R A B A J O     A L U M N O
+# ====================================================================
+
+#----------Definición de Funciones (Dividir)------------
+
+#======================================================================
+#   Algoritmo principal Punto de entrada a la aplicación (Conquistar)
+# =====================================================================
+
+# Ejemplo de funcionamiento
+
+vehiculos_en_parqueadero={}
+memo_park=parq.crear_parqueadero(1,4)
+#park.menu()
+
+puesto_asignado=parq.ocupar_puesto_parqueadero("NAR-110",memo_park,vehiculos_en_parqueadero)
+print("Puesto Asignado",puesto_asignado)
+puesto_asignado=parq.ocupar_puesto_parqueadero("EPM-765",memo_park,vehiculos_en_parqueadero)
+print("Puesto Asignado",puesto_asignado)
+puesto_asignado=parq.ocupar_puesto_parqueadero("MAL-543",memo_park,vehiculos_en_parqueadero)
+print("Puesto Asignado",puesto_asignado)
+puesto_asignado=parq.ocupar_puesto_parqueadero("XLC-113",memo_park,vehiculos_en_parqueadero)
+print("Puesto Asignado",puesto_asignado)
+parq.liberar_puesto_parqueadero("MAL-543",memo_park,vehiculos_en_parqueadero)
+puesto_asignado=parq.ocupar_puesto_parqueadero("EPN-554",memo_park,vehiculos_en_parqueadero)
+print("Puesto Asignado",puesto_asignado)
